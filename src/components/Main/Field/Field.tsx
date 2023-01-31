07c9013fa5aa9803af81d028d9d5dcd675988ff9
Andrey <teryaev.2004@mail.ru>
2023-01-31 13:21:23 +0300

    add-message-layout

--- src/components/Main/Field/Field.tsx
+++ src/components/Main/Field/Field.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import Message from "./Message";
 
 const Field: FC = () => {
   return (
-    <div className="bg-[#758A96] w-[100%] h-[100%]">
-      
+    <div className="bg-[#758A96] w-[100%] h-[100%] flex flex-col">
+      <div className="flex flex-col gap-[5px] mx-[25px] my-[20px] mt-[auto]">
+        <Message message="Привет, как дела?" my={false} />
+        <Message message="Привет, всё ок:)" my={true} />
+      </div>
     </div>
   );
 };
