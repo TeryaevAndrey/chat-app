36fc05099197f6265a68d7be1c00a1c13fee4814
Andrey <teryaev.2004@mail.ru>
2023-02-24 00:16:53 +0300

    adaptive-for-tablet

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,7 +5,7 @@ import Sidebar from "../components/Sidebar/Sidebar";
 const MainPage: FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[60%] h-[80%] bg-white rounded-[7px]">
+      <div className="w-full h-full lg:w-[60%] lg:h-[80%] bg-white rounded-[7px]">
         <div className="flex items-start w-full h-full">
           <Sidebar />
           <Main />
