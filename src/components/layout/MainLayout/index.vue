c7ad4da2f005d4d6405acff61375c43c9d1bdb5b
Andrey <teryaev.2004@mail.ru>
2024-10-09 00:07:12 +0300

    Added daisy ui library and added MainLayout component

---
+++ src/components/layout/MainLayout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="root">
+    <div class="main-block"><slot></slot></div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  @apply bg-base-100 flex justify-center items-center min-h-screen;
+}
+
+.main-block {
+  @apply max-w-4xl xl:max-w-5xl w-full bg-base-200 rounded-3xl h-[80vh];
+}
+</style>
