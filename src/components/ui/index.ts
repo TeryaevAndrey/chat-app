9fc58741c6001d1e44b67f12bf5daa946ad84d41
Andrey <teryaev.2004@mail.ru>
2024-10-09 15:56:17 +0300

    Added Message ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Search } from "./Search/index.vue";
 export { default as ThemeButton } from "./ThemeButton/index.vue";
 export { default as LanguageButton } from "./LanguageButton/index.vue";
+export { default as Message } from "./Message/index.vue";
